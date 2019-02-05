d8e9ae7fa97c747938e5aa7432e4bcf63ecd6b58
Matthew Parkes <matt.parkes@outlook.com>
2019-02-05 13:43:43 +0000

    API progress, simple getters etc

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -5,12 +5,72 @@ const db = require('../../config/database');
 module.exports = function(app) {
     'use strict';
 
-    app.get('/race', function (req, res) {
+    app.get('/raceresult_order', function (req, res) {
         let x = req.query.id;
-        db.getRaceResults(x, res, function(results) {
-            res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+        db.getRaceResultsOrder(x, res, function(results) {
+            res.send(results);
         })
     });
+
+    app.get('/raceresult_normal', function (req, res) {
+        let x = req.query.id;
+        db.getRaceResults(x, res, function(results) {
+            res.send(results);
+        })
+    });
+
+    app.get('/paddler', function (req, res) {
+        let x = req.query.id;
+        db.getPaddler(x, res, function(results) {
+            res.send(results);
+        })
+    });
+
+    app.get('/club', function (req, res) {
+        let x = req.query.id;
+        db.getClub(x, res, function(results) {
+            res.send(results);
+        })
+    })
+
+    app.get('/races', function (req, res) {
+        db.getRaces(res, function(results) {
+            res.send(results);
+        })
+    })
+
+    app.get('/paddlers', function (req, res) {
+        db.getPaddlers(res, function(results) {
+            res.send(results);
+        })
+    })
+
+    app.get('/clubs', function (req, res) {
+        db.getClubs(res, function(results) {
+            res.send(results);
+        })
+    })
+
+    app.get('/clubpaddlers', function (req, res) {
+        let x = req.query.id;
+        db.getClubPaddlers(x, res, function(results) {
+            res.send(results);
+        })
+    })
+
+    app.get('/paddlerstats', function (req, res) {
+        let x = req.query.id;
+        db.getPaddlerStats(x, res, function(results) {
+            res.send(results);
+        })
+    })
+
+    app.get('/paddlerraces', function (req, res) {
+        let x = req.query.id;
+        db.getPaddlerRaces(x, res, function(results) {
+            res.send(results);
+        })
+    })
 
     app.get('/someRoute', function (req, res) {
         res.send('Hello SomeRoute!');
--- config/database.js
+++ config/database.js
@@ -17,8 +17,13 @@ function connectDatabase() {
     return db;
 }
 
-
-function getRaceResults(id, res, callback) {
+/**
+ * This gets a races results in order
+ * @param id
+ * @param res
+ * @param callback
+ */
+function getRaceResultsOrder(id, res, callback) {
     db.query('SELECT raceDivision, `position`, paddler.name, club.clubcode , paddler.class ,paddler.division, ' +
         '`time`,`points`,`pd`,`outcome` FROM `raceresults`, `paddler`, `club` ' +
         'WHERE raceID = ? AND paddler.paddlerID = raceresults.paddlerID AND paddler.clubID = club.clubID ' +
@@ -26,16 +31,138 @@ function getRaceResults(id, res, callback) {
         'when raceresults.position regexp \'^[a-zA-Z]\' then 2 end , raceresults.position + 0 ASC', [id],
         function(err, rows) {
             if (err) {
-                console.log(err);
+                callback(error(err));
             }else {
-                callback(rows);
+                callback(success(rows));
             }
         });
 }
 
+function getRaceResults(id, res, callback) {
+    db.query('SELECT * FROM raceresults WHERE raceID = ?', [id],
+        function(err, rows) {
+            if (err) {
+                callback(error(err));
+            }else {
+                callback(success(rows));
+            }
+        });
+}
 
+function getPaddler(id, res, callback) {
+    db.query('SELECT * FROM paddler WHERE paddlerID = ?', [id],
+        function(err, rows) {
+            if (err) {
+                callback(error(err));
+            }else {
+                callback(success(rows));
+            }
+        });
+}
+
+function getClub(id, res, callback) {
+    db.query('SELECT * FROM club WHERE clubID = ?', [id],
+        function(err, rows) {
+            if (err) {
+                callback(error(err));
+            }else {
+                callback(success(rows));
+            }
+        });
+}
+
+function getRaces(res, callback) {
+    db.query('SELECT * FROM race',
+        function(err, rows) {
+            if (err) {
+                callback(error(err));
+            } else {
+                callback(success(rows));
+            }
+        });
+}
+
+function getPaddlers(res, callback) {
+    db.query('SELECT * FROM paddler',
+        function(err, rows) {
+            if (err) {
+                callback(error(err));
+            } else {
+                callback(success(rows));
+            }
+        });
+}
+
+function getClubs(res, callback) {
+    db.query('SELECT * FROM club',
+        function(err, rows) {
+            if (err) {
+                callback(error(err));
+            } else {
+                callback(success(rows));
+            }
+        });
+}
+
+function getClubPaddlers(id, res, callback) {
+    db.query('SELECT * FROM paddler WHERE clubID = ? ', [id] ,
+        function(err, rows) {
+            if (err) {
+                callback(error(err));
+            } else {
+                callback(success(rows));
+            }
+        });
+}
+
+function getPaddlerStats(id, res, callback) {
+    db.query('SELECT SUM(IF(paddlerID = ?, 1, 0)) as racesEntered,\n' +
+        '           SUM(IF(paddlerID = ? AND (outcome = "DNF" OR outcome = "RTD"), 1, 0)) as retirements,\n' +
+        '           SUM(IF(paddlerID = ? AND outcome = "DNS", 1, 0)) as notStart,\n' +
+        '           SUM(IF(paddlerID = ? AND position = "1", 1, 0)) as first,\n' +
+        '           SUM(IF(paddlerID = ? AND position = "2", 1, 0)) as second,\n' +
+        '           SUM(IF(paddlerID = ? AND position = "3", 1, 0)) as third,\n' +
+        '           SUM(IF(paddlerID = ? AND outcome = "Unknown", 1, 0)) as unknown\n' +
+        'FROM raceresults', [id, id, id, id, id, id ,id] ,
+        function(err, rows) {
+            if (err) {
+                callback(error(err));
+            } else {
+                callback(success(rows));
+            }
+        });
+}
+
+function getPaddlerRaces(id, res, callback) {
+    db.query('SELECT * FROM raceresults WHERE paddlerID = ? ', [id] ,
+        function(err, rows) {
+            if (err) {
+                callback(error(err));
+            } else {
+                callback(success(rows));
+            }
+        });
+}
+
+function success(data){
+    return JSON.stringify({"status": 200, "error": null, "response": data});
+}
+
+function error(error){
+    return JSON.stringify({"status": 500, "error": error, "response": null})
+}
 
 module.exports = {
     connectDatabase: connectDatabase,
-    getRaceResults: getRaceResults
+    getRaceResultsOrder: getRaceResultsOrder,
+    getRaceResults: getRaceResults,
+    getPaddler: getPaddler,
+    getClub: getClub,
+    getRaces: getRaces,
+    getPaddlers: getPaddlers,
+    getClubs: getClubs,
+    getClubPaddlers: getClubPaddlers,
+    getPaddlerStats: getPaddlerStats,
+    getPaddlerRaces: getPaddlerRaces,
+
 };
